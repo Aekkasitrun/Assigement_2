32e723fe87fe7fbc07e3de3d569fc93d453c0ecf
Aekkasitrun <60956669+Aekkasitrun@users.noreply.github.com>
2020-04-21 17:05:40 +0700

    beta0.3

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,7 +1,7 @@
 public class PokemonGame{
     public static void main(String[] args){
         System.out.println("Hello Pokemon Game");
-        Trainer t = new Trainer("chang");
+        Trainer t = new Trainer("JC chang");
         MainGame mg = new MainGame(t);
         //t.play();
         System.out.println("End Pokemon Game"); 
